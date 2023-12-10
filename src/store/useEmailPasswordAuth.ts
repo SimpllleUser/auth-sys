2470ab175953db67bfe6cd856b233a702c3f7e12
v.pecheritsya <v.pecheritsya@upstars.com>
2023-12-10 21:22:27 +0200

    feat: add save data user

--- src/store/useEmailPasswordAuth.ts
+++ src/store/useEmailPasswordAuth.ts
@@ -1,4 +1,4 @@
-import { computed, ref } from 'vue';
+import { computed, ref, watch } from 'vue';
 import { defineStore } from 'pinia';
 
 import { Nullable } from '@/types/base';
@@ -6,11 +6,18 @@ import { Nullable } from '@/types/base';
 import baseAuthService from '../services/base-auth';
 import emailPasswordAuthService, { UserParams } from '@/services/emailPassword-auth';
 import { BaseUserCredential } from '@/services/base-auth';
+import { storage } from '@/services/local-storage';
 
 const { getters } = baseAuthService;
 
+const getInitedUseEmailPasswordStore = () => (storage.getItem('user') ? JSON.parse(storage.getItem('user')) : {});
+
+const saveObjectToStorage = (key: string, value: any) => {
+  storage.setItem(key, JSON.stringify(value));
+};
+
 export const useEmailPasswordStore = defineStore('email-password', () => {
-  const userCredential = ref<Nullable<BaseUserCredential>>(null);
+  const userCredential = ref<Nullable<BaseUserCredential>>(getInitedUseEmailPasswordStore());
 
   const currentUser = computed(getters.currentUser(userCredential));
   const isAuthed = computed(getters.isAuthed(userCredential));
@@ -26,6 +33,13 @@ export const useEmailPasswordStore = defineStore('email-password', () => {
   const signOut = emailPasswordAuthService.signOut(() => {
     userCredential.value = null;
   }, console.log);
+
+  watch(
+    () => userCredential.value,
+    (user) => {
+      saveObjectToStorage('user', user);
+    },
+  );
 
   return {
     signIn,
