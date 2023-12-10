b64c0bd6bbd819677259cd0702b3941d0ebc3a9a
v.pecheritsya <v.pecheritsya@upstars.com>
2023-12-10 12:35:59 +0200

    feat: add .env.local

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,14 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD0zJ8lc_PqWVS2SwGj5K2MgHfaEF0pT9c',
-  authDomain: 'note-d3618.firebaseapp.com',
-  databaseURL: 'https://note-d3618-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'note-d3618',
-  storageBucket: 'note-d3618.appspot.com',
-  messagingSenderId: '26255156252',
-  appId: '1:26255156252:web:c100c472a2fa686e18abd9',
-  measurementId: 'G-XSEV3NBY3X',
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID,
 };
 
 export const firebase = initializeApp(firebaseConfig);
