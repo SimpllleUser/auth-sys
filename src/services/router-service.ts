5a3a8efcfd797b6d2114bfa24e02256cb4d4d5c8
v.pecheritsya <v.pecheritsya@upstars.com>
2023-12-11 20:53:54 +0200

    feat: add base guard for routers and change mode history + mini fix

--- src/services/router-service.ts
+++ src/services/router-service.ts
@@ -1,7 +1,7 @@
 import { RouteLocationNormalized, RouteRecordRaw } from 'vue-router';
 
 type GuardedPath = RouteRecordRaw & { meta: { requiresAuth: boolean } };
-type Route = Array<GuardedPath | RouteRecordRaw>
+type Routes = Array<GuardedPath | RouteRecordRaw>
 
 const makePrivateRoute = (router: RouteRecordRaw): GuardedPath => ({
   ...router,
@@ -12,7 +12,7 @@ export const setRoutesForAuthedUsers = (
   routesForAuthedUsers: Array<RouteRecordRaw>,
 ): Array<RouteRecordRaw> => routesForAuthedUsers.map(makePrivateRoute);
 
-export const initOnChecGuardedRoutes = (routes: Route) => (path: RouteLocationNormalized): boolean => {
+export const initOnChecGuardedRoutes = (routes: Routes) => (path: RouteLocationNormalized): boolean => {
   const actualPath = routes.find(({ name }) => name === path.name);
   return (actualPath && Boolean(actualPath?.meta?.requiresAuth)) || false;
 };
