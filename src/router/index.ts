05dc4b3643d4893b45a25e0de0eea27ce1851bb1
v.pecheritsya <v.pecheritsya@upstars.com>
2023-12-13 22:10:05 +0200

    fefactor: add separator logic for guard path

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import { routes } from '../router/routes';
 import { storage } from '@/services/local-storage';
-import { initOnChecGuardedRoutes } from '../services/router-service';
+import { initOnChecGuardedRoutes, redirectToAccessPage } from '../services/router-service';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -10,23 +10,9 @@ const router = createRouter({
 });
 
 const getUser = () => JSON.parse(storage.getItem('user'));
-
 const isGuardedPath = initOnChecGuardedRoutes(routes);
+// const firstAccesdPageName: string = routes.find(isRequiresAuth)?.name?.toString() || ''
 
-/// TODO before each callback make like method with set default page for redirect and init routes
+router.beforeEach(redirectToAccessPage({ getUser, isGuardedPath }));
 
-router.beforeEach((to, from, next) => {
-  const user = getUser();
-  const isGuarded = isGuardedPath(to);
-
-  if (isGuarded && !user) {
-    if (to.name === 'Login') {
-      next(from);
-    } else {
-      next({ name: 'Login' });
-    }
-  } else {
-    next();
-  }
-});
 export default router;
--- src/services/router-service.ts
+++ src/services/router-service.ts
@@ -1,18 +1,55 @@
-import { RouteLocationNormalized, RouteRecordRaw } from 'vue-router';
+import { NavigationGuardNext, RouteLocationNormalized, RouteRecordRaw } from 'vue-router';
+import { BaseUser } from './base-auth';
 
-type GuardedPath = RouteRecordRaw & { meta: { requiresAuth: boolean } };
-type Routes = Array<GuardedPath | RouteRecordRaw>
+export type GuardedPath = RouteRecordRaw & { meta: { requiresAuth: boolean } };
+export type BaseRoute = GuardedPath | RouteRecordRaw
+export type BaseRoutes = Array<BaseRoute>
+
+interface RedirectToAccessPage {
+  getUser: () => BaseUser
+  isGuardedPath: (path: RouteLocationNormalized) => boolean
+  // firstAccesdPageName: string
+}
+
+const PAGE_NAME_LOGIN = 'Login';
 
 const makePrivateRoute = (router: RouteRecordRaw): GuardedPath => ({
   ...router,
   meta: { ...router.meta, requiresAuth: true },
 });
 
+// export const isRequiresAuth = (route: BaseRoute) => route.meta?.requiresAuth
+
 export const setRoutesForAuthedUsers = (
   routesForAuthedUsers: Array<RouteRecordRaw>,
 ): Array<RouteRecordRaw> => routesForAuthedUsers.map(makePrivateRoute);
 
-export const initOnChecGuardedRoutes = (routes: Routes) => (path: RouteLocationNormalized): boolean => {
+export const initOnChecGuardedRoutes = (
+  routes: BaseRoutes,
+) => (
+  path: RouteLocationNormalized,
+): boolean => {
   const actualPath = routes.find(({ name }) => name === path.name);
   return (actualPath && Boolean(actualPath?.meta?.requiresAuth)) || false;
+};
+
+export const redirectToAccessPage = ({
+  getUser,
+  isGuardedPath,
+}: RedirectToAccessPage) => (
+  to: RouteLocationNormalized,
+  from: RouteLocationNormalized,
+  next: NavigationGuardNext,
+) => {
+  const isGuarded = isGuardedPath(to);
+
+  if (isGuarded && !getUser()) {
+    if (to.name === PAGE_NAME_LOGIN) {
+      next(from);
+    } else {
+      next({ name: PAGE_NAME_LOGIN });
+    }
+  } else {
+    next();
+  }
 };
