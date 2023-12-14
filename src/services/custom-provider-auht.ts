bd1e6df7d27874122e1946f2bb50e8cea107b32f
v.pecheritsya <v.pecheritsya@upstars.com>
2023-12-15 00:36:09 +0200

    feat-todo: add comment for todo

--- src/services/custom-provider-auht.ts
+++ src/services/custom-provider-auht.ts
@@ -2,13 +2,19 @@ import api from './api';
 import { UserParams } from './emailPassword-auth';
 
 const signIn = async (params: UserParams) => {
+  /// TODO add set property token by api
   const result = await api.post('/login', params);
   return result.data;
 };
 
 const signUp = async (params: UserParams) => {
+  /// TODO add set property token by api
   const result = await api.post('/register', params);
   return result.data;
+};
+
+const signOut = () => {
+  /// TODO add reset property token by api
 };
 
 export default {
